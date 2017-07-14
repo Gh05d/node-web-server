8d2ab75f03dba5ded320faa51af5ee761bb6ef48
Gh05d <pacal.clanget@googlemail.com>
2017-07-14 18:01:33 +0200

    Initial Commit for the repository

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', err => {
+    if(err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', text => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    welcomeMessage: 'Welcome to my Website',
+    pageTitle: 'Homepage',
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: {
+      message: 'Site not found',
+      code: '404'
+    }
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000...');
+});
